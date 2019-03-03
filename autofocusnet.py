248de239770975c8df3bfc216d99e12c8e80ddab
Qian Huang <cianhuang@outlook.com>
2019-03-02 23:03:46 -0500

    -

--- autofocusnet.py
+++ autofocusnet.py
@@ -15,7 +15,7 @@ import numpy as np
 import tensorflow as tf
 import h5py
 #from scipy import signal
-from tensorflow.python.keras.layers import Input,Conv2D,Concatenate,Flatten,Dense,LeakyReLU,Dropout
+from tensorflow.python.keras.layers import Input,Conv2D,Concatenate,Flatten,Dense,LeakyReLU,Dropout, ReLU
 from tensorflow.python.keras.callbacks import TensorBoard,ModelCheckpoint
 from tensorflow.python.keras.models import Model
 
@@ -24,10 +24,15 @@ from tensorflow.python.keras import backend as K
 
 from tensorflow.python.keras.activations import relu
 from tensorflow.python.keras.optimizers import Adam
+from tensorflow.python.keras.callbacks import TensorBoard,ModelCheckpoint
 
 #from tensorflow.python.keras.layers import Dense, Dropout, Activation, Flatten
 #from tensorflow.python.keras.layers import Conv2D, MaxPooling2D
 #from tensorflow.python.keras.models import Sequential
+from keras.backend.tensorflow_backend import set_session
+config = tf.ConfigProto()
+config.gpu_options.per_process_gpu_memory_fraction = 0.4
+set_session(tf.Session(config=config))
 
 
 # In[2]:
@@ -46,18 +51,19 @@ batch_size = 28
 # In[3]:
 
 
-train_data_path = 'vcm_data/n_train_data_one_im_norm_2set_512_vcm.npy'
-#test_data_path = 'test_data.npy'
-train_label_path = 'vcm_data/n_train_label_one_im_norm_2set_512_vcm.npy'
-#test_label_path = 'test_label.npy'
+train_data_path = 'train_data_uint8_512_10000_vcm.npy'
+test_data_path = 'real_data_uint8_512_vcm_10000.npy'
+train_label_path = 'train_label_uint8_512_10000_vcm.npy'
+test_label_path = 'real_label_uint8_512_vcm_10000.npy'
 
 
 # In[4]:
 
 
-train_data = np.load(train_data_path)[:10000, :, :, :]
-#test_data = np.load(test_data_path)
-train_label = np.load(train_label_path)[:10000, :]
+train_data = np.load(train_data_path)
+test_data = np.load(test_data_path)
+train_label = np.load(train_label_path)
+test_label = np.load(test_label_path)
 
 
 # In[5]:
@@ -67,7 +73,24 @@ print(train_data.shape)
 print(train_label.shape)
 
 
-# In[12]:
+# In[6]:
+
+
+# import matplotlib.pyplot as plt
+# %matplotlib inline
+# idx = 7
+# plt.imshow(train_data[idx, :, :, 0])
+# print(train_label[idx, :])
+
+
+# In[7]:
+
+
+# plt.imshow(test_data[idx, :, :, 0])
+# print(test_label[idx, :])
+
+
+# In[8]:
 
 
 # In[7]:
@@ -85,47 +108,72 @@ layer3_1 = Conv2D(8, (4, 4), 4,padding='valid',activation=relu, name="Conv3_1")(
 
 flattened = Flatten(name="flat")(layer3_1)
 dense1 = Dense(1024, name="d1")(flattened)
-LReLU1 = LeakyReLU(0.1, name="lr1")(dense1)
+ReLU1 = LeakyReLU(0.1, name="lr1")(dense1)
 #dense1 = Dropout(0.8)(dense1)
 
-dense2 = Dense(512, name="d2")(LReLU1)
-LReLU2 = LeakyReLU(0.1, name="lr2")(dense2)
+dense2 = Dense(512, name="d2")(ReLU1)
+ReLU2 = LeakyReLU(0.1, name="lr2")(dense2)
 #dense2 = Dropout(0.5)(dense2)
 
-dense3 = Dense(10, name="d3")(LReLU2)
-LReLU3 = LeakyReLU(0.1, name="lr3")(dense3)
+dense3 = Dense(10, name="d3")(ReLU2)
+ReLU3 = LeakyReLU(0.1, name="lr3")(dense3)
 
-output_position = Dense(1, name="out")(LReLU3)
+output_position = Dense(1, name="out")(ReLU3)
 print(output_position)
 
 model = Model(inputs=input_image1, outputs=output_position)
 model.summary()
 
+tcbc = TensorBoard(log_dir='1')
 
+filepath="models/autofocus_train_on_generate/{epoch:03d}-{val_loss:.2f}.hdf5"
+checkpoint = ModelCheckpoint(filepath, monitor='val_loss', verbose=1, save_best_only=False, mode='max',period=2)
 
 print(model.output_shape)
 
 
-# In[7]:
+# In[9]:
+
+
+def data_gen(features, labels, batch_size):
+    while True:
+        for i in np.arange(0, features.shape[0] - batch_size, batch_size):
+            # choose random index in features
+            batch_features = features[i:i+batch_size, :, :, :].astype('float16')/255.0
+            batch_labels = abs(labels[i:i+batch_size, 1:2] - labels[i:i+batch_size, :1])/100
+            yield (batch_features, batch_labels)
+
+
+# In[13]:
 
 
 opt = Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0001)
 model.compile(loss='mse', optimizer=opt)
 
-model.fit(train_data[:,:,:,0:1].astype('float16')/255.0, abs(train_label[:,1:2]-train_label[:,0:1])/100, epochs = epochs,batch_size = batch_size, validation_split = 0.2, verbose=1)
+# model.fit(train_data[:,:,:,0:1].astype('float16')/255.0, abs(train_label[:,1:2]-train_label[:,0:1])/100, 
+#           epochs = 100,batch_size = batch_size, #validation_split=0.2,
+#           validation_data=(test_data[:,:,:,0:1].astype('float16')/255.0, abs(test_label[:,1:2]-test_label[:,0:1])/100), 
+#           verbose=1, callbacks = [tcbc, checkpoint])
+model.fit_generator(data_gen(train_data, train_label, batch_size), 
+                    steps_per_epoch = train_data.shape[0]/batch_size, epochs = epochs,
+                    validation_data=data_gen(test_data, test_label, batch_size), 
+                    validation_steps = batch_size,
+                    verbose=1, callbacks = [tcbc, checkpoint])
 
+
+# In[ ]:
 
 
 model.save('my_model_512.h5')
 
 
-# In[8]:
+# In[ ]:
 
 
 # print(train_label[:, :10])
 
 
-# In[9]:
+# In[ ]:
 
 
 # import matplotlib.pyplot as plt
@@ -133,14 +181,14 @@ model.save('my_model_512.h5')
 # plt.imshow(train_data[1, :, :, 0])
 
 
-# In[10]:
+# In[ ]:
 
 
 # from tensorflow.python.keras.models import load_model
 # model = load_model('my_model_step1.h5')
 
 
-# In[11]:
+# In[ ]:
 
 
 # model.predict(train_data[1:2, :, :, :1])
--- disnet.py
+++ disnet.py
@@ -25,7 +25,13 @@ from tensorflow.python.keras import backend as K
 from tensorflow.python.keras.activations import relu
 from tensorflow.python.keras.optimizers import Adam
 from tensorflow.python.keras.utils import to_categorical
+
 from sklearn.model_selection import train_test_split
+
+from keras.backend.tensorflow_backend import set_session
+config = tf.ConfigProto()
+config.gpu_options.per_process_gpu_memory_fraction = 0.4
+set_session(tf.Session(config=config))
 
 
 # In[2]:
@@ -44,85 +50,121 @@ batch_size = 28
 # In[3]:
 
 
-train_data_path = 'vcm_data/train_data_one_im_norm_2set_512_vcm_discrinimator.npy'
-#test_data_path = 'test_data.npy'
-train_label_path = 'vcm_data/train_label_one_im_norm_2set_512_vcm_discrinimator.npy'
-#test_label_path = 'test_label.npy'
+train_data_path = 'train_data_uint8_512_vcm_discrinimator.npy'
+test_data_path = 'real_data_uint8_512_vcm_10000.npy'
+train_label_path = 'train_label_uint8_512_vcm_discrinimator.npy'
+test_label_path = 'real_label_uint8_512_vcm_10000.npy'
 
 
 # In[4]:
 
 
-data = np.load(train_data_path)[:10000, :, :, :]
-#test_data = np.load(test_data_path)
-label = np.load(train_label_path)[:10000, :]
+train_data = np.load(train_data_path)
+test_data = np.load(test_data_path)
+train_label = np.load(train_label_path)
+test_label = np.load(test_label_path)
+test_label = (abs(test_label[:, 0] - test_label[:, 1]) <= 33)
 
 
 # In[5]:
 
 
-print(data.shape)
-print(label.shape)
-
-
-# In[10]:
-
-
-train_data = data
-train_label = label
+# train_data = data[:10000, :, :, :]
+# train_label = label[:10000, :]
 
 
 # In[7]:
 
 
-#to_categorical(train_label[:, 2]).shape
+# #to_categorical(train_label[:, 2]).shape
+# import matplotlib.pyplot as plt
+# %matplotlib inline
+# idx = 7
+# plt.imshow(train_data[idx, :, :, 0])
+
+# print(train_label[idx, :])
 
 
-# In[24]:
+# In[8]:
+
+
+# plt.imshow(test_data[idx, :, :, 0])
+
+# print(test_label[idx])
+
+
+# In[9]:
 
 
 # In[7]:
 input_image1 = Input(shape=(512,512,1), name = "input")
 #layer1_1 = Conv2D(4, (8, 8), 8,padding='valid',activation=None,use_bias=False,kernel_initializer = my_init,trainable=True,name='layer1')(input_image1)
-layer1_1 = Conv2D(8, (9, 9), 4,padding='same',activation=relu, name="Conv1_1")(input_image1)
+layer1_1 = Conv2D(1, (8, 8), 8,padding='same',activation=relu, name="Conv1_1")(input_image1)
 
-layer2_1 = Conv2D(16, (5, 5), 2,padding='same',activation=relu, name="Conv2_1")(layer1_1)
+layer2_1 = Conv2D(1, (8, 8), 8,padding='same',activation=relu, name="Conv2_1")(layer1_1)
 
-layer3_1 = Conv2D(32, (3, 3), 2,padding='same',activation=relu, name="Conv3_1")(layer2_1)
+# layer3_1 = Conv2D(32, (3, 3), 2,padding='same',activation=relu, name="Conv3_1")(layer2_1)
 
-layer4_1 = Conv2D(32, (3, 3), 2,padding='same',activation=relu, name="Conv4_1")(layer3_1)
+# layer4_1 = Conv2D(32, (3, 3), 2,padding='same',activation=relu, name="Conv4_1")(layer3_1)
 
-flattened = Flatten(name="flat")(layer4_1)
-#Dp1 = Dropout(0.5)(flattened)
+flattened = Flatten(name="flat")(layer2_1)
+
 dense1 = Dense(10, name="d1")(flattened)
-#ReLU1 = ReLU(name="lr1")(dense1)
+Dp1 = Dropout(0.5)(dense1)
+ReLU1 = ReLU(name="lr1")(Dp1)
 
-output_position = Dense(1, activation='sigmoid', name="out")(dense1)
+output_position = Dense(2, activation='softmax', name="out")(ReLU1)
 
 print(output_position)
 
 model = Model(inputs=input_image1, outputs=output_position)
 model.summary()
 
+tcbc = TensorBoard(log_dir='1')
+
+filepath="models/dis_train_on_generate/{epoch:03d}-{val_loss:.2f}.hdf5"
+checkpoint = ModelCheckpoint(filepath, monitor='val_loss', verbose=1, save_best_only=False, mode='max',period=2)
+
 
 print(model.output_shape)
 
 
-# In[33]:
+# In[10]:
+
+
+def data_gen(features, labels, batch_size):
+    while True:
+        for i in np.arange(0, features.shape[0] - batch_size, batch_size):
+            # choose random index in features
+            batch_features = features[i:i+batch_size, :, :, :].astype('float16')/255.0
+            batch_labels = labels[i:i+batch_size, :]
+            yield (batch_features, batch_labels)
+
+
+# In[11]:
 
 
 opt = Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0001)
 model.compile(loss='binary_crossentropy', optimizer=opt)
 
-model.fit(train_data.astype('float16')/255.0, train_label[:, 2:], epochs = epochs,batch_size = batch_size,verbose=1, validation_split = 0.2)
-
-model.save('my_model_dis_512.h5')
+# model.fit(train_data[:,:,:,0:1].astype('float16')/255.0, to_categorical(train_label[:, 2:]), 
+#           epochs = 100,batch_size = batch_size,
+#           validation_data=(test_data[:,:,:,0:1].astype('float16')/255.0, to_categorical(test_label)), 
+#           verbose=1, callbacks = [tcbc, checkpoint])
+model.fit_generator(data_gen(train_data, to_categorical(train_label[:, 2:]), batch_size), 
+                    steps_per_epoch = train_data.shape[0]/batch_size, epochs = epochs,
+                    validation_data=data_gen(test_data, to_categorical(test_label), batch_size), 
+                    validation_steps = batch_size,
+                    verbose=1, callbacks = [tcbc, checkpoint])
 
 
 # In[ ]:
 
 
+model.save('my_model_dis_512.h5')
+
 model.save_weights('my_dis_weights_512.h5')
+# # model.load_weights('my_dis_weights_512.h5')
 
 
 # In[ ]:
@@ -131,16 +173,16 @@ model.save_weights('my_dis_weights_512.h5')
 # print(to_categorical(train_label[:10, 2:]))
 
 
-# In[27]:
+# In[ ]:
 
 
-# pred  = model.predict(train_data[:, :, :, :10])
+# pred  = model.predict(train_data[:, :, :, :50])
 
 
-# In[32]:
+# In[ ]:
 
 
-# idx = 5
+# idx = 25
 # plt.imshow(train_data[idx, :, :, 0])
 
 # print(train_label[idx, :])
