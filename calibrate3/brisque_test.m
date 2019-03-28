b2e4130edd6f4dbd00d07cde8b487fbf99492d79
Qian Huang <cianhuang@outlook.com>
2019-03-28 12:02:43 -0400

    -

---
+++ calibrate3/brisque_test.m
@@ -0,0 +1,7 @@
+I = imread('13_1000_32232_s00_00000.jpg');
+I2 = imread('13_825_31710_s00_00000.jpg');
+I3 = imread('13_500_30743_s00_00000.jpg');
+
+brisque(I)
+brisque(I2)
+brisque(I3)
--- calibrate_raw/quadratic.m
+++ calibrate_raw/quadratic.m
@@ -2,21 +2,26 @@ clc; clear; close all;
 
 keySet = {'1000','950', '875', '825','800','725','700', '675','650',...
     '625', '600','575', '550','525','500','475'};
-figure;
-y = 0;
+%figure;
+%y = 0;
 for i = 1: length(keySet)
+   Model_parameters_BGR = zeros(22, 11);
    load(strcat('Model_parameters_B_', keySet{i}, '.mat'));
-   scatter(Model_parameters_B(:, 4), (Model_parameters_B(:, 3)), [], 'B');
+   Model_parameters_BGR(:, 1:5) = Model_parameters_B;
+%   scatter(Model_parameters_B(:, 4), (Model_parameters_B(:, 3)), [], 'B');
 %    y = y + Model_parameters(:, 4);
-   hold on;
+%   hold on;
   load(strcat('Model_parameters_G_', keySet{i}, '.mat'));
-   scatter(Model_parameters_G(:, 4), (Model_parameters_G(:, 3)), [], 'G');
+  Model_parameters_BGR(:, 6:8) = Model_parameters_G(:, 3:5);
+%   scatter(Model_parameters_G(:, 4), (Model_parameters_G(:, 3)), [], 'G');
 %    y = y + Model_parameters(:, 4);
-   hold on;
+%   hold on;
   load(strcat('Model_parameters_R_', keySet{i}, '.mat'));
-   scatter(Model_parameters_R(:, 4), (Model_parameters_R(:, 3)), [], 'R');
+    Model_parameters_BGR(:, 9:11) = Model_parameters_R(:, 3:5);
+%   scatter(Model_parameters_R(:, 4), (Model_parameters_R(:, 3)), [], 'R');
 %    y = y + Model_parameters(:, 4);
-   hold on;
+%   hold on;
+    save(strcat('Model_parameters_BGR_',keySet{i},'.mat'),'Model_parameters_BGR');  
 end
 % x = Model_parameters(:, 2);
 % y = y/10-1;   
