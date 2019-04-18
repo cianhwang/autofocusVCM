206d38389533559914d9f55c72ab3688b2e3ccea
Qian Huang <cianhuang@outlook.com>
2019-04-18 10:42:14 -0400

    -

--- calibrate_raw/quadratic.m
+++ calibrate_raw/quadratic.m
@@ -5,15 +5,28 @@ keySet = {'1000','950', '875', '825','800','725','700', '675','650',...
 
 %% alpha-s
 % figure;
-% y = 0;
-% for i = 1:16
-%     
-%    load(strcat('Model_parameters_BGR_', keySet{i}, '.mat'));
+y = 0;
+xx = []
+yy = []
+for i = 1:16
+    
+   load(strcat('Model_parameters_BGR_', keySet{i}, '.mat'));
 %    scatter(Model_parameters_BGR(:, 2), (Model_parameters_BGR(:, 4)));
-%    y = y + Model_parameters_BGR(:, 4);
+   xx = [xx; Model_parameters_BGR(:, 2)];
+   yy = [yy; Model_parameters_BGR(:, 4)-min(Model_parameters_BGR(:, 4))];
+   y = y + Model_parameters_BGR(:, 4);
 %    hold on;
-%  
-% end
+ 
+end
+figure, 
+scatter(xx,yy)
+hold on
+[p,S] = polyfit(xx, yy, 2);
+x1 = linspace(475, 1000);
+[y1, delta] = polyval(p,x1,S);
+plot(x1,y1+2*delta,'m--',x1,y1-2*delta,'m--')
+axis([475, 1000, 0, 0.1])
+
 % figure;
 % x = Model_parameters_BGR(:, 2);
 % y = y / 16;
@@ -31,31 +44,48 @@ keySet = {'1000','950', '875', '825','800','725','700', '675','650',...
 
 %% alpha-r
 %  figure,
+%  x = [];
+%  y = [];
 % for i = 2:15
-% %     if i == 7
-% %        continue; 
-% %     end
-% 
 %    load(strcat('Model_parameters_BGR_', keySet{i}, '.mat'));
-%    scatter(abs(Model_parameters_BGR(2:end-1, 4)-1), (Model_parameters_BGR(2:end-1, 9)), [], 'b');
+%    x = [x;abs(Model_parameters_BGR(2:end-1, 4)-1)];
+%    y = [y;Model_parameters_BGR(2:end-1, 3)];
 %    %axis([0 0.1 0 50])
-%     hold on;
 % end
+% scatter(y, x, [], 'b');
+% hold on;
+% [p,S] = polyfit(y, x, 1);
+% y1 = linspace(0, 50);
+% [x1, delta] = polyval(p,y1,S);
+% plot(y1,x1+2*delta,'m--',y1,x1-2*delta,'m--')
+% axis([0 50 0 0.1])
+% title('Blue channel (focal step: 500-950)')
+% ylabel('1-\alpha')
+% xlabel('r')
+% legend(strcat('delta =',num2str(delta(1),3)));
+% 
+% figure,
+% scatter(x, y, [], 'b');
+% hold on;
+% [p,S] = polyfit(x, y, 1);
+% x1 = linspace(0, 50);
+% [y1, delta] = polyval(p,x1,S);
+% plot(x1,y1+2*delta,'m--',x1,y1-2*delta,'m--')
+% axis([0 0.1 0 50])
 % title('Blue channel (focal step: 500-950)')
 % xlabel('1-\alpha')
 % ylabel('r')
+% legend(strcat('delta =',num2str(delta(1))));
 
 %% visualize 0.01
-I = imread("5_800_10093_s01_00000.jpg");
-[m n ~] = size(I);
-I = I(m/2-256-111:m/2+255-111, n/2-256-111:n/2+255-111);
-%figure, imshow(I);
-h = fspecial('disk',3);
-I2 = imfilter(I, h);
-%figure, imshow(I2)
-I3 = imfilter(I2, h);
-h2 = fspecial('disk',4);
-I4 = imfilter(I, h2);
-montage({I3, I4})
-
-
+% I = imread("5_800_10093_s01_00000.jpg");
+% [m n ~] = size(I);
+% I = I(m/2-256-111:m/2+255-111, n/2-256-111:n/2+255-111);
+% figure, imshow(I);
+% h = fspecial('disk',3);
+% I2 = imfilter(I, h);
+% figure, imshow(I2)
+% I3 = imfilter(I2, h);
+% h2 = fspecial('disk',4);
+% I4 = imfilter(I, h2);
+% montage({I3, I4})
