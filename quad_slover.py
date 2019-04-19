863a464df454e5b080e52ac2dfe1bcf120349aa2
Qian Huang <cianhuang@outlook.com>
2019-04-19 11:42:17 -0400

    -

--- quad_slover.py
+++ quad_slover.py
@@ -3,6 +3,8 @@ import numpy as np
 def solver(curr_pos, delta_y):
     a1 = 0.04476
     a2 = -1.869e-5
+#   a1 = 0.04655
+#   a2 = -1.9481e-5
     b = 2*a2*curr_pos+a1
     d1 = (b)**2 + 4*a2*delta_y
     d2 = (b)**2 - 4*a2*delta_y
